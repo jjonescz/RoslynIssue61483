117ef6afab45b944a69a3198219cc019146ea556
Jan Jones <janjones@microsoft.com>
2023-03-07 17:05:41 +0100

    Add third benchmark

--- Benchmarks.cs
+++ Benchmarks.cs
@@ -3,41 +3,38 @@ using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 public partial class Program
 {
 	static void Main(string[] args)
 	{
-        var job = Job.ShortRun;
+		var job = Job.ShortRun;
 		BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, ManualConfig.CreateMinimumViable()
-            .HideColumns("Error", "StdDev", "Median", "RatioSD")
-            .AddDiagnoser(new DisassemblyDiagnoser(new DisassemblyDiagnoserConfig(printSource: true)))
+			.HideColumns("Error", "StdDev", "Median", "RatioSD")
+			.AddDiagnoser(new DisassemblyDiagnoser(new DisassemblyDiagnoserConfig(printSource: true)))
 			.AddJob(job.WithCustomBuildConfiguration("ReleaseCustomRoslyn"))
 			.AddJob(job.AsBaseline()));
 	}
 
-	[Benchmark, ArgumentsSource(nameof(GetArguments))]
-    public int Compare1(int x, int y)
-    {
-        if (x < y) return -1;
-        if (x > y) return 1;
-        return 0;
-    }
+	[Benchmark, Arguments(1, 2)]
+	public int Compare1(int x, int y)
+	{
+		if (x < y) return -1;
+		if (x > y) return 1;
+		return 0;
+	}
 
-    [Benchmark, ArgumentsSource(nameof(GetArguments))]
-    public int Compare2(int x, int y)
-    {
-        int tmp1 = (x > y) ? 1 : 0;
-        int tmp2 = (x < y) ? 1 : 0;
-        return tmp1 - tmp2;
-    }
+	[Benchmark, Arguments(1, 2)]
+	public int Compare2(int x, int y)
+	{
+		int tmp1 = (x > y) ? 1 : 0;
+		int tmp2 = (x < y) ? 1 : 0;
+		return tmp1 - tmp2;
+	}
 
-    public IEnumerable<object[]> GetArguments()
-    {
-        var rnd = new Random(42);
-        return Enumerable.Range(0, 1).Select(i => new object[] { rnd.Next(), rnd.Next() });
-    }
+	[Benchmark, Arguments('A')]
+	public int Compare3(char c)
+	{
+		return (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z') ? 1 : 0;
+	}
 }
